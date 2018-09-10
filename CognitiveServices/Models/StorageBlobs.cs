418773aa716d4c641749072b4ed514a3c99856dc
Mlondi <21521600@dut4life.ac.za>
2018-09-10 09:18:55 +0200

    first commit

---
+++ CognitiveServices/Models/StorageBlobs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CognitiveServices.Models
+{
+    public class StorageBlobs
+    {
+        public string ImageUri { get; set; }
+        public string ThumbnailUri { get; set; }
+        public string Caption { get; set; }
+    }
+}
